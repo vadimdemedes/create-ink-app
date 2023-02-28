d73c1ab2e72ade3a33cf4329f503fe81b562dbe1
Kai Hao <kevin830726@gmail.com>
2023-03-01 03:15:19 +0800

    Create app in a new directory (#11)

--- cli.js
+++ cli.js
@@ -1,19 +1,46 @@
 #!/usr/bin/env node
 'use strict';
+const path = require('path');
 const meow = require('meow');
 const createInkApp = require('.');
 
-meow(`
+const cli = meow(
+	`
 	Options
 		--typescript		Use TypeScript React template
 
 	Usage
-		$ mkdir my-cli
-		$ cd my-cli
-	  $ create-ink-app
-`);
+		$ create-ink-app <project-directory>
 
-createInkApp().catch(error => {
-	console.error(error.stack);
-	process.exit(1);
-});
+	Examples
+		$ create-ink-app my-cli
+		$ create-ink-app .
+`,
+	{
+		flags: {
+			typescript: {
+				type: 'boolean'
+			}
+		}
+	}
+);
+
+const projectDirectoryPath = path.resolve(process.cwd(), cli.input[0] || '.');
+
+createInkApp(projectDirectoryPath)
+	.catch(error => {
+		console.error(error.stack);
+		process.exit(1);
+	})
+	.then(() => {
+		const pkgName = path.basename(projectDirectoryPath);
+		const relativePath = path.relative(process.cwd(), projectDirectoryPath);
+
+		console.log();
+		console.log('Ink app created! Get started with:');
+		console.log();
+		if (relativePath) {
+			console.log(`  cd ${relativePath}`);
+		}
+		console.log(`  ${pkgName}`);
+	});
